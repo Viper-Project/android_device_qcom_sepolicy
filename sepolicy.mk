aosp-forking/device_qcom_sepolicy

sepolicy: Fix path for custom
Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

sepolicy.mk
@@ -24,4 +24,4 @@ BOARD_SEPOLICY_DIRS += \
 # Add sepolicy version to support OS upgrade and backward compatibility
 BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
 
--include device/lineage/sepolicy/qcom/sepolicy.mk
+-include device/custom/sepolicy/qcom/sepolicy.mk

Viper-Project/android_device_qcom_sepolicy

sepolicy.mk
@@ -27,4 +27,4 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy-legacy-um/private
 
--include device/lineage/sepolicy/qcom/sepolicy.mk
+-include device/viper/sepolicy/qcom/sepolicy.mk